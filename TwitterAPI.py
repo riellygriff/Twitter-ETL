ddeadbcab87f8950f48b6b2471caf17d36da81d2
Rielly Griffiths <75856115+riellygriff@users.noreply.github.com>
2022-08-16 09:48:01 -0600

    Update TwitterAPI.py

--- TwitterAPI.py
+++ TwitterAPI.py
@@ -7,11 +7,6 @@ from Creds import BEARER_TOKEN
 
 load_dotenv()
 
-bearer_token = os.getenv('BEARER_TOKEN')
-consumer_key = os.getenv('API_KEY')
-consumer_secret = os.getenv('API_KEY_SECRET')
-access_key = os.getenv('ACCESS_TOKEN')
-access_secret = os.getenv('ACCESS_TOKEN_SECRET')
 
 def user_tweets(user):
     client = tweepy.Client(bearer_token=BEARER_TOKEN)
@@ -22,10 +17,4 @@ def user_tweets(user):
                                        tweet_fields=['created_at', 'author_id', 'public_metrics'],
                                        max_results=50)
     return response
-    # for tweet in response.data:
-    #     print(tweet.id)
-    #     print(tweet.author_id)
-    #     print(tweet.created_at)
-    #     print(tweet.text)
-    #     print(tweet.public_metrics['retweet_count'])
-    #     print(tweet.public_metrics['like_count'])
+
